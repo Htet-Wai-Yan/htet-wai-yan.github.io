c9923d1d8576ec8c41d458f293437270ca28ffd9
Htet-Wai-Yan <h.waiyan101@gmail.com>
2021-01-27 22:35:27 +0630

    Changed header and skill section

--- jQuery/animation.js
+++ jQuery/animation.js
@@ -2,11 +2,68 @@
 $(document).ready(function(){
   
   $(function(){
-    // image filp animation
-    $(".flip").flip({
-        trigger: 'click'
-    })
+    // // image filp animation
+    // $(".flip").flip({
+    //     trigger: 'click'
+    // })
     // text typing animation
-    $('.greeting h3').typedText('I am a Front-End Developer', 80)
-  });
+    $('.grid-items-1 h2').typedText('Front-End Developer', 80)
+    // carousel
+    $('.slider').slick({
+      arrows: false,
+      autoplay: true,
+      autoplay: 3000,
+      dots: false,
+      infinite: true,
+      // mobileFirst: true,
+    })
+    // circle progress bar
+    jQuery(function($) {
+      $('.bootstrap').circleProgress({
+        max: 100,
+        value: 80,
+        textFormat: 'percent',
+      });
+    })
+
+    jQuery(function($) {
+      $('.jQuery').circleProgress({
+        max: 100,
+        value: 60,
+        textFormat: 'percent',
+      });
+    })
+
+    jQuery(function($) {
+      $('.react').circleProgress({
+        max: 100,
+        value: 0,
+        textFormat: 'percent',
+      });
+    })
+
+    jQuery(function($) {
+      $('.nodeJs').circleProgress({
+        max: 100,
+        value: 0,
+        textFormat: 'percent',
+      });
+    })
+
+    jQuery(function($) {
+      $('.flutter').circleProgress({
+        max: 100,
+        value: 40,
+        textFormat: 'percent',
+      });
+    })
+
+    jQuery(function($) {
+      $('.php').circleProgress({
+        max: 100,
+        value: 10,
+        textFormat: 'percent',
+      });
+    })
+  })
 });
